1fd6f73bb14e4bd3efa9e482036453682a1afc1c
jumpei527 <jumpei5278@gmail.com>
2024-08-30 17:45:06 +0900

    Add some tests of invalid requests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,38 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'asdfasdfasdf',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'asdfasdfasdf',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('returns a 400 missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'asdfasdf',
+    })
+    .expect(400);
+});
