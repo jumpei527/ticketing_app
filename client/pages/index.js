00041c09073ea9094ef3232387d4e7047a409f0b
jumpei527 <jumpei5278@gmail.com>
2024-09-02 01:01:50 +0900

    Create header

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,11 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  return currentUser ? <h1>You are signin</h1> : <h1>You are NOT signin</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
