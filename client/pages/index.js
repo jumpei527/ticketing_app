5cd600c6192a10645599bde6244d6ebd6290998d
jumpei527 <jumpei5278@gmail.com>
2024-09-01 22:50:15 +0900

    Add build-client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const Landing = () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+
   return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
