37c85a37217912ad720b93d3fc315a14592c029f
jumpei527 <jumpei5278@gmail.com>
2024-09-13 12:31:18 +0900

    Add ticket-created-event

---
+++ nats-test/src/events/ticket-created-event.ts
@@ -0,0 +1,10 @@
+import { Subjects } from './ticket-created-listener';
+
+export interface TicketCreatedEvent {
+  subject: Subjects.TicketCreated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+  };
+}
