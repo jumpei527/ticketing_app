523935ab7579b61f82bea39f28763018dc906348
jumpei527 <jumpei5278@gmail.com>
2024-09-13 17:01:46 +0900

    Add ticket-created-publisher

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from '@jp_tickets/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
