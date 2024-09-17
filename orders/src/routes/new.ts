7ad6465465a0e3f726c4d03a72a0eb5393a89373
jumpei527 <jumpei5278@gmail.com>
2024-09-17 11:11:15 +0900

    Add isReserved

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -34,18 +34,8 @@ router.post(
     }
 
     // Make sure that this ticket is not already reserved
-    // Run query to look at all orders
-    const existingOrder = await Order.findOne({
-      ticket: ticket,
-      status: {
-        $in: [
-          OrderStatus.Created,
-          OrderStatus.AwaitingPayment,
-          OrderStatus.Complete,
-        ],
-      },
-    });
-    if (existingOrder) {
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
       throw new BadRequestError('Ticket is already reserved');
     }
 
