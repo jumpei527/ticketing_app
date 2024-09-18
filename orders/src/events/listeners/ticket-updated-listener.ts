4137808c2c40fa060dc60bc34e0ed056a6c88a87
jumpei527 <jumpei5278@gmail.com>
2024-09-19 00:31:05 +0900

    Add ticket-updated-listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'nats-node-streaming';
+import { Subjects, Listener, TicketUpdatedEvent } from '@jp_tickets/common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMesssage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
