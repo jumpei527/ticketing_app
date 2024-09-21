c7191969befe4dfef123f02bbec2f8f3f21cba21
jumpei527 <jumpei5278@gmail.com>
2024-09-21 12:03:16 +0900

    Update order-created-listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,7 +3,6 @@ import { Listener, OrderCreatedEvent, Subjects } from '@jp_tickets/common';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
 import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publishers';
-import { natsWrapper } from '../../nats-wrapper';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -23,7 +22,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Save the ticket
     await ticket.save();
-    new TicketUpdatedPublisher(natsWrapper.client);
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
 
     // ack the message
     msg.ack();
