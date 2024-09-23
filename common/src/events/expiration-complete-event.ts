9fb50cd9479d9aa49a290fc1c7d9df53560363a3
jumpei527 <jumpei5278@gmail.com>
2024-09-23 18:40:55 +0900

    Update expiration-complete-event

--- common/src/events/expiration-complete-event.ts
+++ common/src/events/expiration-complete-event.ts
@@ -3,6 +3,6 @@ import { Subjects } from './subjects';
 export interface ExpirationCompleteEvent {
   subject: Subjects.ExpirationComplete;
   data: {
-    orderid: string;
+    orderId: string;
   };
 }
