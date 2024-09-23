ae8bb7948c62034a7461a898276c5854a28f606e
jumpei527 <jumpei5278@gmail.com>
2024-09-23 16:41:22 +0900

    Update package.json

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@jp_tickets/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
