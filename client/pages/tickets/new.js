7f39ff6d9402abdda3b3e2d3979e85b8a1eb6adf
jumpei527 <jumpei5278@gmail.com>
2024-09-27 00:45:46 +0900

    Update new to show index

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,8 +1,25 @@
 import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const NewTicket = () => {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/tickets',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
 
   const onBlur = () => {
     const value = parseFloat(price);
@@ -17,7 +34,7 @@ const NewTicket = () => {
   return (
     <div>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
@@ -35,8 +52,9 @@ const NewTicket = () => {
             className="form-control"
           />
         </div>
+        {errors}
+        <button className="btn btn-primary">Submit</button>
       </form>
-      <button className="btn btn-primary">Submit</button>
     </div>
   );
 };
