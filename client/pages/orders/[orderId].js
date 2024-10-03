de9de9c70403c7b917e0ec403e216ebb70816f8f
jumpei527 <jumpei5278@gmail.com>
2024-10-03 16:01:04 +0900

    Add stripeCheckout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -20,7 +21,17 @@ const OrderShow = ({ order }) => {
   if (timeLeft < 0) {
     return <div>Order Expired</div>;
   }
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51Q38vgP6jpzPiwwXY8pT9CR6aIXCCw1UCjLG0wJ4ilMIjl4LmzRSbGYxxnGIpSiiW2UwdmWl7ankB1r9G8DvCyBq00ewgKq1yl"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
